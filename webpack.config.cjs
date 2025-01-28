b7a410175d86fed57e748aa7540427a65e2c289c
Lum Dukaj <lum@gjirafa.com>
2025-01-28 14:53:07 +0100

    more updates

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,37 +1,35 @@
 const path = require("path");
 
-module.exports = () => {
-	return {
-		entry: ["./src/index.js", "./src/style.scss"],
-		output: {
-			filename: "main.js",
-			path: path.resolve(__dirname, "dist"),
-			library: {
-				type: "module",
+module.exports = {
+	entry: ["./src/index.js", "./src/style.scss"],
+	output: {
+		filename: "main.js",
+		path: path.resolve(__dirname, "dist"),
+		library: {
+			type: "module",
+		},
+	},
+	experiments: {
+		outputModule: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: "babel-loader",
 			},
-		},
-		experiments: {
-			outputModule: true,
-		},
-		module: {
-			rules: [
-				{
-					test: /\.js$/,
-					exclude: /node_modules/,
-					use: "babel-loader",
-				},
-				{
-					test: /\.scss$/,
-					use: [
-						"style-loader", // Injects styles into the DOM
-						"css-loader", // Translates CSS into CommonJS modules
-						"sass-loader", // Compiles SCSS into CSS
-					],
-				},
-			],
-		},
-		resolve: {
-			extensions: [".js", ".scss"],
-		},
-	};
+			{
+				test: /\.scss$/,
+				use: [
+					"style-loader", // Injects styles into the DOM
+					"css-loader", // Translates CSS into CommonJS modules
+					"sass-loader", // Compiles SCSS into CSS
+				],
+			},
+		],
+	},
+	resolve: {
+		extensions: [".js", ".scss"],
+	},
 };
